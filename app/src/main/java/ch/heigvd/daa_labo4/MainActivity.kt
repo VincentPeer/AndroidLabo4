83b7851e0b2452deff75a24eadb0d26a124b14c3
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-12-09 09:50:10 +0100

    Add layouts

--- app/src/main/java/ch/heigvd/daa_labo4/MainActivity.kt
+++ app/src/main/java/ch/heigvd/daa_labo4/MainActivity.kt
@@ -1,11 +1,31 @@
 package ch.heigvd.daa_labo4
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        val view = super.onCreateView(name, context, attrs)
+
+        if (view != null) {
+            val recycler = view.findViewById<RecyclerView>(R.id.recycler)
+            val adapter = ListAdapter()
+            recycler.adapter = adapter
+            recycler.layoutManager = GridLayoutManager(this, 3)
+        }
+
+        return view
+    }
+
+
 }
